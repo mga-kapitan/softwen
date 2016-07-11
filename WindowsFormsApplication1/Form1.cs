0a7c7c409e75cb7d2a93aae41592a2349c573756
Kosaki Onodera <r00t.localhost@hotmail.com>
2016-07-11 16:10:05 +0800

    Added Messagebox

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            MessageBox.Show("Test");
         }
     }
 }
